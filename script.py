72449d6adff500dccfa44d4ccd9ff49b14a9d332
anakyouzogolov <64495296+anakyouzogolov@users.noreply.github.com>
2020-07-27 15:39:48 -0700

    Add files via upload

---
+++ script.py
@@ -0,0 +1,40 @@
+import rarfile
+from rarfile import RarWrongPassword
+import random
+
+# chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+# chars_list = list(chars)
+
+rarfile.UNRAR_TOOL = "C:\\Program Files\\WinRAR\\UnRaR.exe"
+
+
+rar_file = input("Enter your rar file name :")
+
+r = rarfile.RarFile(f'{rar_file}.rar')
+
+guess_password = ""
+
+
+while(True):
+
+    guess_password = str(random.randint(0, 999))
+
+    print("*****", guess_password, "******")
+    
+    try:
+        r.extractall(pwd=guess_password)
+        print("RaR Password is : ", guess_password)
+        break
+    except RarWrongPassword as err:
+        print("error", err)
+        continue
+
+
+
+
+    
+   
+
+       
+
+
